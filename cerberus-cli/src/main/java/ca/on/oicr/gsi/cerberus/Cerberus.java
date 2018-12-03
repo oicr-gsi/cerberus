1ba17c05f264c26491c28df60cea9bebff386c2f
iainrb <1695061+iainrb@users.noreply.github.com>
2018-12-03 13:30:38 -0500

    Move XML config out of cerberus-core, and other fixups

--- cerberus-cli/src/main/java/ca/on/oicr/gsi/cerberus/Cerberus.java
+++ cerberus-cli/src/main/java/ca/on/oicr/gsi/cerberus/Cerberus.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.zip.GZIPOutputStream;
@@ -76,10 +77,10 @@ public class Cerberus {
             System.exit(0);
         }
 
-        Boolean error = false;
+        boolean error = false;
         // check required options are present
-        ArrayList<String> required = new ArrayList<>();
-        ArrayList<String> missing = new ArrayList<>();
+        List<String> required = new ArrayList<>();
+        List<String> missing = new ArrayList<>();
         required.add(ACTION_OPT);
         required.add(OUT_OPT);
         required.add(TYPE_OPT);
@@ -133,7 +134,6 @@ public class Cerberus {
 
         ProvenanceHttpClient client = new ProvenanceHttpClient(uri);
 
-        // TODO validate inputs, eg. type and action are in respective enums
         try {
             Map<String, Set<String>> incFilters = null;
 
